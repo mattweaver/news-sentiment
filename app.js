0aea5b572aa2e1c99b2cf5ea36a4d71c6cbc7d12
mattweaver <Glorange1>
2017-12-21 15:37:34 +0000

    Progress made but issues with non matching article search results

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var newsSentiment = require('./news-sentiment');
 
-
 var keywords = [
     'Bitcoin',
     'Cryptocurrency',
@@ -12,14 +11,27 @@ var keywords = [
 
 var rssFeeds = [
     'https://www.theverge.com/rss/index.xml',
+    'https://www.theverge.com/tech/rss/index.xml',
     'http://feeds.bbci.co.uk/news/technology/rss.xml',
     'http://feeds.reuters.com/reuters/technologyNews',
     'http://feeds.reuters.com/reuters/UKBankingFinancial',
     'http://feeds.skynews.com/feeds/rss/technology.xml',
     'http://feeds.skynews.com/feeds/rss/business.xml',
     'https://news.bitcoin.com/feed/'
-
-
 ]
 
-newsSentiment.parseFeeds(rssFeeds, keywords)
+var webNews = [{
+    linkListUrl: 'https://www.theverge.com/search?q=cryptocurrency+bitcoin+litecoin+ethereum',
+    linkListUrlMarker: '.c-entry-box--compact__title a',
+    articleContentMarker: '. l-col__main .c-entry-content',
+    requiredURLString: ''
+}, {
+    linkListUrl: 'https://www.bbc.co.uk/search?q=cryptocurrency',
+    linkListUrlMarker: '.search-results h1 a',
+    articleContentMarker: '.story-body__inner',
+    requiredURLString: 'www.bbc.co.uk/news'
+}]
+
+
+//newsSentiment.parseFeeds(rssFeeds, keywords)
+newsSentiment.scrapeNews(webNews)
--- news-sentiment.js
+++ news-sentiment.js
@@ -1,6 +1,13 @@
 var parser = require('rss-parser');
-var sentiment = require('sentiment');
+var sentiment1 = require('sentiment');
+var sentiment2 = require('sentiment-analysis');
 var q = require('q');
+var scraperjs = require('scraperjs');
+var removeNewline = require('newline-remove');
+
+process.on('uncaughtException', function(err) {
+    console.log(err);
+})
 
 module.exports = {
 
@@ -22,12 +29,75 @@ module.exports = {
             console.log('Found ' + matchingEntries.length + ' relevant articles');
             console.log('-------------------')
             for (j = 0; j < matchingEntries.length; j++) {
-                console.log('SENTIMENT: ' + sentiment(matchingEntries[j].content).score + ' -- ' + matchingEntries[j].title + ' -- ' + matchingEntries[j].link);
+                console.log('SENTIMENT 1: ' + sentiment1(matchingEntries[j].content).score + ' -- SENTIMENT 2: ' + sentiment2(matchingEntries[j].content) + ' -- ' + matchingEntries[j].title + ' -- ' + matchingEntries[j].link);
             }
         })
+    },
+    scrapeNews: function(webNews) {
+        var promises = [];
+        for (var i = 0; i < webNews.length; i++) {
+            promises.push(scrapeData(webNews[i].linkListUrl, webNews[i].linkListUrlMarker, 'href'));
+        };
+        q.allSettled(promises).then(function(results) {
+            var articleURLs = [];
+            for (var i = 0; i < results.length; i++) {
+                for (j = 0; j < results[i].value.content.length; j++) {
+                    articleURLs = articleURLs.concat({ articleURL: results[i].value.content[j], parentURL: results[i].value.parentURL })
+                }
+            }
 
+            articleURLs = filterRequiredURLStrings(webNews, articleURLs);
+
+            var promises = [];
+            for (var i = 0; i < articleURLs.length; i++) {
+                var deferred = q.defer();
+                scrapeData(articleURLs[i].articleURL, getParentURLContentMarker(webNews, articleURLs[i].parentURL), 'text').then(function(data) {
+                    var contentConcat = "";
+                    for (var j = 0; j < data.content.length; j++) {
+                        contentConcat = contentConcat + removeNewline(data.content[j]);
+                    }
+                    deferred.resolve({ parentURL: data.parentURL, content: contentConcat })
+                });
+                promises.push(deferred.promise);
+            }
+            //console.log(promises)
+            q.allSettled(promises).then(function(results) {
+                for (var i = 0; i < results.length; i++) {
+                    console.log(results[i].value)
+                }
+            })
+        });
     }
+}
 
+var getParentURLContentMarker = function(webNews, parentURL) {
+    for (var i = 0; i < webNews.length; i++) {
+        if (webNews[i].linkListUrl == parentURL)
+            return webNews[i].articleContentMarker;
+    }
+    return undefined;
+}
+var filterRequiredURLStrings = function(webNews, articleURLs) {
+    var filteredArticleURLs = [];
+    try {
+        console.log(articleURLs)
+        for (var i = 0; i < articleURLs.length; i++) {
+            console.log('xxxxxx')
+            console.log(articleURLs[i])
+            for (var j = 0; j < webNews.length; j++) {
+                if (webNews[j].linkListUrl == articleURLs[i].parentURL) {
+                    if (articleURLs[i].articleURL.indexOf(webNews[j].requiredURLString) != -1) {
+                        filteredArticleURLs.push(articleURLs[i]);
+                    }
+                }
+            }
+
+            console.log(filteredArticleURLs)
+            return filteredArticleURLs;
+        }
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 var parseUrl = function(rssFeed, keywords) {
@@ -62,3 +132,34 @@ var keywordMatcher = function(entry, keywords) {
     matchingEntries = removeDuplicateObjects(matchingEntries, 'title')
     return removeDuplicateObjects(matchingEntries, 'title');
 };
+
+var scrapeData = function(url, marker, contentType) {
+    var deferred = q.defer();
+    try {
+        var scraperjs = require('scraperjs');
+        scraperjs.StaticScraper.create(url)
+            .scrape(function($) {
+                return $(marker).map(function() {
+                    switch (contentType) {
+                        case 'text':
+                            return $(this).text();
+                            break;
+                        case 'href':
+                            return $(this).attr('href');
+                            break;
+                    }
+                }).get();
+            })
+            .then(function(content) {
+                deferred.resolve({
+                    content: content,
+                    parentURL: url
+                });
+            })
+    } catch (err) {
+        console.log(err)
+    }
+    return deferred.promise;
+}
+
+//scrapeData('http://www.bbc.co.uk/news/world-asia-india-42390013', '.story-body__inner', 'text')
